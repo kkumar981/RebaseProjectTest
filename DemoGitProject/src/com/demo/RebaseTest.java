5f66d78d0e6785f909d64ecaac131fd85a6b239e
Rakesh Kumar <kkumar981@gmail.com>
2018-03-08 10:58:21 +0530

    Second commit from Master

--- DemoGitProject/src/com/demo/RebaseTest.java
+++ DemoGitProject/src/com/demo/RebaseTest.java
@@ -5,6 +5,7 @@ public class RebaseTest {
 	public static void main(String[] args) {
 		System.out.println("Hi Git");
 		System.out.println("First commit from Master");
+		System.out.println("Second commit from Master");
 	}
 
 }
